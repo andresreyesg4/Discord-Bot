5efcef6eabeb0ea432c0e5f0cc7812c2a1bae96a
Andres Reyes <71036855+andresreyesg4@users.noreply.github.com>
2021-08-08 17:45:47 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,22 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main{
     // create an instance to read the token
     public static JDA jda;
     // initialize the token reader.
-    static Token token = new Token();
+    static Properties properties = new Properties();
     // Main method
-    public static void main(String[] args) throws LoginException{
+    public static void main(String[] args) throws LoginException, IOException {
         // Initialize the API to the discord bot in the channel.
-        jda = JDABuilder.createDefault(token.getDiscordToken()).build();
+        InputStream in = Main.class.getResourceAsStream("/" + "tokens.properties");
+        properties.load(in);
+        jda = JDABuilder.createDefault(properties.getProperty("discord.token")).build();
         // register event listener.
-        jda.addEventListener(new Youtube());
+        jda.addEventListener(new youtube());
     }
 }
